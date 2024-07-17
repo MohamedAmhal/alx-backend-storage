59983331fadd4193cc8e1a4142089c0601ca9eca
MohamedAmhal <mohammedamhal349@gmail.com>
2024-07-17 16:43:52 +0100

    adding the first task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,7 +23,7 @@ class Cache:
         '''
         self._redis = redis.Redis()
         self._redis.flushdb(True)
-    
+
     def store(self, data: Union[str, int, float, bytes]) -> str:
         '''
         the methode store that take a variable data
